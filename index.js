facaec6fe3e7f8f5ae83cba19869aa62f809d2d6
Joseph Aleman <isjoecool@yahoo.com>
2024-01-19 10:26:54 -0800

    added partition at top

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+console.log("-----------------------------PART 1---------------------------------------------");
 let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 
 // initialize currrentCell and cells Variables
